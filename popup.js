615ec0a1267e655fedcab3d30bc16207d17c6968
DavidGtzDev <davidgtzleal@gmail.com>
2022-09-17 19:57:55 -0600

    first commit

---
+++ popup.js
@@ -0,0 +1,12 @@
+const btnSelect = document.getElementById("button_select")
+const ttlContainer = document.getElementById("title_container")
+
+
+
+btnSelect.addEventListener("click", async() => {
+    ttlContainer.style.display = "none"
+    btnSelect.style.display = "none"
+
+
+})
+
